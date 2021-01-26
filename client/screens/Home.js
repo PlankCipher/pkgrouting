c8b781cb53e01ebb9919916d5702f2850d427f54
PlankCipher <plankcipher1@protonmail.com>
2021-01-26 18:46:36 +0200

    Put marker on map and save coords on user press; Add clear markers button

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+import { FontAwesome5 } from '@expo/vector-icons';
 import SafeAreaViewCrossPlatform from '../components/SafeAreaViewCrossPlatform.js';
 
 class Home extends Component {
@@ -11,6 +14,26 @@ class Home extends Component {
       latitude: 0,
       longitude: 0,
     },
+    stops: [],
+  };
+
+  handleMapPress = (event) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+
+    this.setState({
+      stops: [
+        ...this.state.stops,
+        { id: uuidv4(), coords: { latitude, longitude } },
+      ],
+    });
+  };
+
+  handleStopMarkerPress = (currentId) => {
+    const newStops = this.state.stops.filter(({ id }) => id != currentId);
+
+    this.setState({
+      stops: newStops,
+    });
   };
 
   _getUserLocation = async () => {
@@ -25,15 +48,20 @@ class Home extends Component {
       } = await Location.getCurrentPositionAsync();
 
       this.setState({
-        region: {
+        location: {
           latitude,
           longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
         },
       });
 
-      this.mapView.animateToRegion(this.state.region, 700);
+      this.mapView.animateToRegion(
+        {
+          ...this.state.location,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+        700,
+      );
     }
   };
 
@@ -46,6 +74,7 @@ class Home extends Component {
   render() {
     const {
       location: { latitude, longitude },
+      stops,
     } = this.state;
 
     return (
@@ -56,8 +85,36 @@ class Home extends Component {
           initialRegion={{
             latitude,
             longitude,
+            latitudeDelta: Number.MAX_VALUE,
+            longitudeDelta: Number.MAX_VALUE,
           }}
-        />
+          showsUserLocation={true}
+          loadingEnabled={true}
+          moveOnMarkerPress={false}
+          onPress={this.handleMapPress}
+          onPoiClick={this.handleMapPress}
+        >
+          {stops.map(({ id, coords }) => {
+            return (
+              <Marker
+                coordinate={coords}
+                // NOTE: should we use stopPropagation={true} to stop
+                // NOTE: iOS map from re-placing markers when pressed?
+                key={id}
+                onPress={() => this.handleStopMarkerPress(id)}
+              />
+            );
+          })}
+        </MapView>
+        {stops.length > 0 && (
+          <TouchableWithoutFeedback
+            onPress={() => this.setState({ stops: [] })}
+          >
+            <View style={styles.clearButtonOuter}>
+              <FontAwesome5 style={styles.clearButton} name="trash-alt" solid />
+            </View>
+          </TouchableWithoutFeedback>
+        )}
       </SafeAreaViewCrossPlatform>
     );
   }
@@ -65,6 +122,32 @@ class Home extends Component {
 
 const styles = StyleSheet.create({
   map: { height: '100%' },
+  clearButtonOuter: {
+    width: 40,
+    height: 40,
+    borderRadius: 40 / 2,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    position: 'absolute',
+    bottom: 25,
+    right: 21,
+    backgroundColor: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+  },
+  clearButton: {
+    fontSize: 24,
+    color: 'red',
+  },
 });
 
 export default Home;
